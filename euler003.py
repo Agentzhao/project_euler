1875baf31df916740e68df5bb6d6dec92803f886
Agentzhao <tan.hongzhao@dhs.sg>
2018-01-31 10:08:38 +0800

    Update euler003.py

--- euler003.py
+++ euler003.py
@@ -7,3 +7,15 @@ while i * i < n:
     i = i + 1
 
 print (n)
+
+#------function version-------
+def lpfactor(number):
+    
+    i = 2
+    
+    while i * i < number:
+        while number%i == 0:
+            n = n / i
+        i = i + 1
+    
+    return n
