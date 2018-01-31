3d4b09f1ae31b17288850bbb3d40d1f8ac7ef5ef
Agentzhao <tan.hongzhao@dhs.sg>
2018-01-31 10:07:49 +0800

    Update euler002.py

--- euler002.py
+++ euler002.py
@@ -19,3 +19,27 @@ for i in range(0, len(even)):
     del even[0]
 
 print(total)
+
+
+#--------function version---------
+def even_fibonacci(limit):
+    numbers = [1,2]
+    while numbers[-1] < limit:
+      numbers.append(numbers[-1]+numbers[-2])
+    del numbers[-1]
+    
+    #find all the even numbers
+    even = []
+    for i in range (0,len(numbers)):
+      if numbers[i]%2 == 0:
+          even.append(numbers[i])
+      else:
+          continue
+    
+    #find the total
+    total = 0
+    for i in range(0, len(even)):
+      total = total + even[0]
+      del even[0]
+    
+    return total
